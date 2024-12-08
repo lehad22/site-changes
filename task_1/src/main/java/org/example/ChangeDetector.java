0c8944ecda140a2f443236a12367c7ddadcf1ba1
lehad22 <lehad22002@gamil.com>
2024-12-08 22:17:50 +0700

    fix

--- task_1/src/main/java/org/example/ChangeDetector.java
+++ task_1/src/main/java/org/example/ChangeDetector.java
@@ -6,18 +6,29 @@ import java.util.Map;
 import java.util.Set;
 
 public class ChangeDetector {
-// В данном случае ? extends URL не нужен, так как URl final, но по хорошему сделать так
+    // В данном случае ? extends URL не нужен, так как URl final, но по хорошему сделать так
     public static Changes detectChanges(Map<? extends URL, String> yesterday, Map<? extends URL, String> today) {
-        Set<URL> disappeared = new HashSet<>(yesterday.keySet());
-        disappeared.removeAll(today.keySet());
 
-        Set<URL> appeared = new HashSet<>(today.keySet());
-        appeared.removeAll(yesterday.keySet());
-
+        Set<URL> disappeared = new HashSet<>();
+        Set<URL> appeared = new HashSet<>();
         Set<URL> changed = new HashSet<>();
-        for (URL url : yesterday.keySet()) {
-            if (today.containsKey(url) && !yesterday.get(url).equals(today.get(url))) {
-                changed.add(url);
+
+        for (Map.Entry<? extends URL, String> entry : yesterday.entrySet()) {
+            URL url = entry.getKey();
+            String oldContent = entry.getValue();
+            if (!today.containsKey(url)) {
+                disappeared.add(url);
+            }
+            else {
+                String newContent = today.get(url);
+                if (!oldContent.equals(newContent)) {
+                    changed.add(url);
+                }
+            }
+        }
+        for (URL url : today.keySet()) {
+            if (!yesterday.containsKey(url)) {
+                appeared.add(url);
             }
         }
 
